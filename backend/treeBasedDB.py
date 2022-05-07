42f6d06c2197fb157a69eebf8020401d65792eae
Andrew Emerson <32277829+AndrewDEmerson@users.noreply.github.com>
2022-05-07 08:08:28 -0700

    RB Tree

--- backend/treeBasedDB.py
+++ backend/treeBasedDB.py
@@ -1,20 +1,39 @@
+from random import randint, randrange
+import random
 from typing import List, Tuple
+from rbtree import RBtree
 
 
 class dataBase:
     def __init__(self):
         # Creation of the database
         # Should load all Word - Popularity combos into the tree
-        print("\033[91mWARNING:\033[00m  Loading Data Into Tree Not Yet Implemented.")
-        
+        self.myTree = RBtree()
+        self.keys = list()
+        with open("database.csv", "r") as file:
+            for line in file:
+                kvpair = line.split(",")
+                self.myTree.insert(kvpair[0],int(kvpair[1]))
+                self.keys.append(kvpair[0])
+        #print("\033[91mWARNING:\033[00m  Loading Data Into Tree Not Yet Implemented.")
+                
     def getWords(self, n=2) -> List[str]:
         # return N random words from the dataset
         # Note, we may also want to try storing the words(without their matching keys) as an array for O(1) access of any random element
-        print("\033[91mWARNING:\033[00m  getWords not yet implemented.")
-        return ["apple","bannana"]
+        #print("\033[91mWARNING:\033[00m  getWords not yet implemented.")
+        return random.sample(self.keys, n)
     
     def getMostPopular(self, words) -> Tuple[str, List[int]]:
         # return the most popular word in the form of a string
         # as well as a list of each words score
-        print("\033[91mWARNING:\033[00m  getMostPopular not yet implemented.")
-        return "apple", [0,1]
+        bestWord = words[0]
+        bestValue = 0
+        allvalues = list()
+        #print("\033[91mWARNING:\033[00m  getMostPopular not yet implemented.")
+        for word in words:
+            val = self.myTree.GetValue(word)
+            if val > bestValue:
+                bestValue = val
+                bestWord = word
+            allvalues.append(val)
+        return bestWord,allvalues
